3ab3214bb8bf5e2b1f4c31ae089d7397014e99ed
Lucas Crijns <lucascrijns@gmail.com>
2020-10-02 11:35:20 +0200

    Updated the creation of Taylor functions to accept at paremeter

--- taylorexpansion/tools.py
+++ taylorexpansion/tools.py
@@ -8,7 +8,7 @@ def flatten_function(func, n_input, n_output):
     """
     return lambda x: tf.reshape(func(tf.reshape(x, (1, n_input))), (n_output,))
 
-def create_function_vector_vector(coeffs, n_input, n_output):
+def create_function_vector_vector(coeffs, n_input, n_output, at):
     """
     Creates a function from a Taylor expansion
     """
@@ -21,13 +21,16 @@ def create_function_vector_vector(coeffs, n_input, n_output):
                     counts = Counter(path)
                     prod = 1*v
                     for nth, t in counts.items():
-                        prod *= x[nth]**t
+                        prod *= (x[nth]-at[nth])**t
                     total += prod
             y[i] = total
         return y
     return func
 
 def batch_vectorize(func, n_output):
+    """
+    Transforms a function in to accepting a batch of shape (batch_size, ...)
+    """
     def fun(x):
         y = np.zeros((len(x), n_output))
         for index, i in enumerate(x):
