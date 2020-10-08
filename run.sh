55cd54fde0d6f92aaaa1ac678adf2612707f4461
Lucas Crijns <lucascrijns@gmail.com>
2020-10-08 17:32:35 +0200

    Added custom taylor object and improved function speed with Python expression

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run as user instead of root to prevent permission issues
-docker run -it --rm -v $(realpath $(pwd)):/tmp -w /tmp -u $(id -u):$(id -g) honours python ./$1
+docker run -it --rm -v $(realpath $(pwd)):/tmp -w /tmp -u $(id -u):$(id -g) -e TF_CPP_MIN_LOG_LEVEL=2 honours python ./$1
 
