0af3791de8ff3f21d31d0cf16fedbd035539da70
Lucas Crijns <lucascrijns@gmail.com>
2020-10-09 00:01:39 +0200

    2D test.py

--- taylorexpansion/test.py
+++ taylorexpansion/test.py
@@ -9,25 +9,25 @@ tf.random.set_seed(1)
 
 # Feel free to adjust this value and see how the approximation becomes better
 n = 5
-at = [0.0, 1.0]
+at = [0.0, 10.0]
 n_input = 2
 n_output = 1
 
 model = keras.Sequential([
-    layers.Dense(5, name="input", input_shape=(n_input,), activation="sigmoid"),
-    layers.Dense(5, activation="tanh"),
-    layers.Dense(n_output, name="output")
+    layers.Dense(8, name="input", input_shape=(n_input,), activation="sigmoid"),
+    layers.Dense(12, activation="sigmoid"),
+    layers.Dense(n_output)
     ])
 
 func = Taylor(model, at, n_input, n_output, n, is_batch=True)
 
-# points = np.linspace(-2, 2)
+points = np.array([np.linspace(-2, 2), np.zeros(50)]).T
 
-# batch_points = tf.reshape(points, (-1, 1))
-# yg = tf.reshape(model(batch_points), (-1,)).numpy()
-# yw = np.reshape(func(batch_points, batched=True), (-1,))
+batch_points = tf.reshape(points, (-1, 2))
+yg = tf.reshape(model(batch_points), (-1,)).numpy()
+yw = np.reshape(func(batch_points, batched=True), (-1,))
 
-# plt.plot(points, yw, label="Taylor")
-# plt.plot(points, yg, label="Model")
-# plt.legend()
-# plt.savefig("test.jpg")
+plt.plot(points[:, 0], yw, label="Taylor")
+plt.plot(points[:, 0], yg, label="Model")
+plt.legend()
+plt.savefig("test.jpg")
