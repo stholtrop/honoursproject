19537774f80c368d5d4e3e52bf46d5f882a25e88
Lucas Crijns <lucascrijns@gmail.com>
2020-10-17 15:50:03 +0200

    Updated test.py

--- taylorexpansion/test.py
+++ taylorexpansion/test.py
@@ -8,17 +8,13 @@ import numpy as np
 
 tf.random.set_seed(1)
 
-model = keras.Sequential([
-    layers.Dense(5, name="input", input_shape=(1,), activation="sigmoid"),
-    layers.Dense(5, activation="tanh"),
-    layers.Dense(1, name="output")
-    ])
+model = lambda x: 1/(1+x)
 
 
 new_func = flatten_function(model, 1, 1)
 
 # Feel free to adjust this value and see how the approximation becomes better
-n = 5
+n = 6
 at = [0.0]
 n_input = 1
 n_output = 1
@@ -27,13 +23,19 @@ coeffs = taylor_coefficients_vector_vector(new_func, n_input, n_output, at, n)
 print(pretty_print_taylor_vector(coeffs))
 taylor_func = create_function_vector_vector(coeffs, n_input, n_output, at)
 batch_taylor_func = batch_vectorize(taylor_func, n_output)
-points = np.linspace(-2, 2)
+points = np.linspace(-5, 5)
 
-batch_points = tf.reshape(points, (-1, 1))
+x = np.exp(-points)
+batch_points = tf.reshape(x, (-1, 1))
 yg = tf.reshape(model(batch_points), (-1,)).numpy()
 yw = np.reshape(batch_taylor_func(batch_points), (-1,))
 
+div = points[np.where(np.abs(yw-yg) > 0.01)]
+print(div)
+
 plt.plot(points, yw, label="Taylor")
 plt.plot(points, yg, label="Model")
+plt.ylim((-0.5, 1.5))
+plt.grid()
 plt.legend()
 plt.savefig("test.jpg")
