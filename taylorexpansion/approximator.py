7d0067e82529eac56c4984925af19b40b7fb6941
Lucas Crijns <lucascrijns@gmail.com>
2020-11-29 12:42:00 +0100

    Updated signature of approximator

--- taylorexpansion/approximator.py
+++ taylorexpansion/approximator.py
@@ -10,21 +10,35 @@ def normalize(un_input, bounds):
     Returns:
         (np.ndarray(n_samples, n_input)): Normalized input
     """
-    (un_input - bounds[:, 0])/bounds[:, 1]
+    return (un_input - bounds[:, 0])/(bounds[:, 1]-bounds[:, 0])
 
 
 class Approximator:
-    def __init__(self, network, samples):
+    def __init__(self, network, n_input, n_output, samples, n_terms, bounds):
         """Initialize the Approximator class with a network and samples
 
         Args:
             network (lambda x(np.ndarray(n_samples, n_input)): y(np.ndarray(n_samples, n_output))): The to be approximated
                         neural network
+            n_input (int): number of inputs to the network
+            n_output (int): number of outputs of the network
             samples (np.ndarray(n_points, n_input)): Set of points on which to expand the function
+            n_terms (int): number of taylor terms for the Approximator
+            bounds (np.ndarray(n_input, 2)): Bounds for the input normalization
         Returns:
             Approximator object
         """
-        pass
+        self.network = network
+        self.n_input = n_input
+        self.n_output = n_output
+        self.samples = samples
+        self.n_terms = n_terms
+        self.bounds = bounds
+        # Construct Taylor network
+        self.taylor = {}
+        for point in self.samples:
+            self.taylor[point] = Taylor(self.network, point, self.n_input, self.n_output, self.n_terms, is_batch=True, default_batch=True)
+
     def __call__(self, input_data):
         """Calls underlying taylor network, in batched mode
 
@@ -33,7 +47,7 @@ class Approximator:
         Returns:
             np.ndarray(n_samples, n_output)
         """
-        pass
+        
     def compare(self, input_data):
         """Compares the initially given neural network with the Taylor network
 
